e82bdac6c1299723b539537ed94725e2f7ab00cb
hana <hanahemati9@gmail.com>
2025-12-24 13:32:05 +0330

    Create Team model with owner and members relationships

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+
+class Teams(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_teams')
+    members = models.ManyToManyField(User, related_name='teams')
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
