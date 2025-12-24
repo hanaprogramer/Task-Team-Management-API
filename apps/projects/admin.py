bfac024acad10fa341cf41dc957f4174c141e9ee
hana <hanahemati9@gmail.com>
2025-12-24 14:40:03 +0330

    Create Project model with relationships register Project model in admin and test in admin panels

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
 # Register your models here.
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.teams.models import *
+from apps.users.models import *
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    team = models.ForeignKey(Teams, on_delete=models.CASCADE, related_name='projects')
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateField(null=True, blank=True)    
+    end_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
