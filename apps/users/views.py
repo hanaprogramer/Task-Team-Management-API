05993dea4b5b11a238a245d2ddef572c4f5c97c7
hana <hanahemati9@gmail.com>
2025-12-25 16:13:19 +0330

    feat: add user registration endpoint

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,8 +3,16 @@ from . serializers import *
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.views import APIView
+from rest_framework import status
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(RegisterSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
