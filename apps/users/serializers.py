6f7f1174e762fbb297dd06167f58cc3ea1362a7e
hana <hanahemati9@gmail.com>
2025-12-25 11:19:26 +0330

    Add RegisterSerializer for User with secure password handling and default role

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,5 +1,7 @@
 from .models import *
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +14,30 @@ class UserSerializer(serializers.ModelSerializer):
             'role',
             'is_active',
         ]
+
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role', 'is_active']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'role': {'read_only': True, 'default': 'member'},
+            'is_active': {'read_only': True},
+        }
+
+    def validate_password(self, value):
+        validate_password(value)  # اینجا بررسی امنیتی انجام میشه
+        return value
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            role='member',
+            is_active=True
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
