287cb091810cad047a82af39a5da2f76f372a738
hana <hanahemati9@gmail.com>
2026-01-03 10:32:52 +0330

    fix(comments): improve Comment __str__ representation

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -40,4 +40,4 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"Comment by {self.author.username} on {self.task.title}"
